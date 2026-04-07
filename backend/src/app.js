18f1ac59bff2bb84d01377811565d30c86e95866
Abhiram <168246653+Abhiram-5656@users.noreply.github.com>
2026-04-07 09:19:30 +0530

    Update allowed origin for CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(
     origin: (origin, callback) => {
       const allowedOrigins = [
         config.frontendUrl,
-        "https://ems-1-v8rq.onrender.com",
+        "https://ems-1-v8rq.onrender.com/api",
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:5175',
